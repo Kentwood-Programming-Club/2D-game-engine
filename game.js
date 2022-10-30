3d67eb47e452d8aec320258504cb272f06e5d218
Peter Dodge <ptr.dodge@gmail.com>
2022-10-30 11:04:56 -0700

    keyboard stuff

--- game.js
+++ game.js
@@ -1,1 +1,12 @@
 // this is the file where all the code goes
+import { Keyboard } from "./lib/keyboard";
+
+let wasd = ["a", "d", "w", "s"] // left, right, up, down
+
+let kb = new Keyboard()
+kb.keymap(wasd, [left, right, up, down])
+
+var left = () => console.log("left")
+var right = () => console.log("right")
+var up = () => console.log("up")
+var down = () => console.log("down")
