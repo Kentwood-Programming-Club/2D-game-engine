d62f0e95e023484d150eba5aa8bde6d3677fa0f1
Vlad <vlappleman@icloud.com>
2022-11-04 14:54:27 -0700

    fixed frameRate calculation revised comment

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ export let canvas = document.querySelector("#game")
 export let ctx = canvas.getContext("2d")
 
 // // we need a loop to update the game 30 times a second
-let frameRate = 30 // milliseconds
+let frameRate = 1000/ 30 // time that we wait in between frames in milliseconds (1000 milliseconds / 30 frames)
 window.requestAnimationFrame(gameLoop)
 
 function gameLoop() {
